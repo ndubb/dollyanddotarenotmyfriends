ed22854764df76f17ad8482ae15656555357ee0c
norageddon <thegirlwhoismadeofhats.hotmail.com>
2020-06-29 21:33:19 -0400

    Added better support for named sound groups (to expand later), added mutehelp commands, cleaned up UI printing

--- DollyAndDotMute.lua
+++ DollyAndDotMute.lua
@@ -1,135 +1,118 @@
--- Dolly and Dot are Not My Friends (v1.1 - Updated 2020-06-28)
-local DollyAndDotIDs = {
-    1998845, 3169894
-}
-local TrainIDs = {
-	-- BLOOD ELF TRAIN
-	539203, 539219, 1313588, 1306531, 
-	-- DARK IRON TRAIN
-	1902030, 1902543,
-	-- DRAENEI TRAIN
-	539516, 539730,
-	-- DWARF TRAIN
-	539802, 539881,
-	-- GNOME TRAIN
-	540271, 540275,
-	-- GOBLIN TRAIN
-	542017, 541769, 
-	-- HIGHMOUNTAIN TRAIN
-	1730534, 1730908,
-	-- HUMAN TRAIN
-	540734, 540535,
-	-- KUL TIRAN TRAIN
-	2491898, 2531204,
-	-- LIGHTFORGED TRAIN
-	1731656, 1731282, 
-	-- MAGHAR TRAIN
-	1951457, 1951458, 
-	-- MECHAGNOME TRAIN
-	3107182, 3107651, 
-	-- NIGHT ELF TRAIN
-	540947, 540870, 1316209, 1304872,
-	-- NIGHTBORNE TRAIN
-	1732405, 1732030,
-	-- ORC TRAIN
-	541157, 541239,
-	-- PANDAREN TRAIN
-	636621, 630296, 630298,
-	-- TAUREN TRAIN
-	542896, 542818,
-	-- TROLL TRAIN
-	543085, 543093,
-	-- UNDEAD TRAIN
-	542600, 542526,
-	-- VOID ELF TRAIN
-	1732785, 1733163,
-	-- VULPERA TRAIN
-	3106252, 3106717, 
-	-- WORGEN/GILNEAN TRAIN
-	542206, 542035, 541463, 541601,
-	-- ZANDALARI TRAIN
-	1903049, 1903522
-}
+-- Dolly and Dot are Not My Friends (v1.2 - Updated 2020-06-29)
 
--- TODO: TRACKING THESE IN CASE WOW EVER ADDS SUPPORT FOR DISABLING VIA SOUNDKIT
--- local SoundKitIDs = {
-	-- -- DOLLY AND DOT ARE MY BEST FRIENDS
-	-- 105996, 143176,
-	-- -- BLOOD ELF TRAIN
-	-- 9672, 9644, 56584, 57024,
-	-- -- DARK IRON TRAIN
-	-- 99880, 101903, 100051, 101977, 
-	-- -- DRAENEI TRAIN
-	-- 9697, 9722, 
-	-- -- DWARF TRAIN
-	-- 7637, 7636, 
-	-- -- GNOME TRAIN
-	-- 7640, 7641, 
-	-- -- GOBLIN TRAIN
-	-- 19147, 19257,
-	-- -- HIGHMOUNTAIN TRAIN
-	-- 91543, 95560, 95882, 91732, 
-	-- -- HUMAN TRAIN
-	-- 7634, 7635, 
-	-- -- KUL TIRAN TRAIN
-	-- 124869, 127053, 124149, 127147, 
-	-- -- LIGHTFORGED TRAIN
-	-- 91921, 96196, 92108, 96264, 
-	-- -- MAGHAR TRAIN
-	-- 102483, 110340, 102484, 110415, 
-	-- -- MECHAGNOME TRAIN
-	-- 141367, 144291, 141212, 143908,
-	-- -- NIGHT ELF TRAIN
-	-- 7642, 7643, 56285, 57202, 
-	-- -- NIGHTBORNE TRAIN
-	-- 92296, 96332, 96400, 92483, 
-	-- -- ORC TRAIN
-	-- 7638, 7638, 
-	-- -- PANDAREN TRAIN
-	-- 29826, 28932,
-	-- -- TAUREN TRAIN
-	-- 7647, 7646, 
-	-- -- TROLL TRAIN 
-	-- 7649, 7648, 
-	-- -- UNDEAD TRAIN
-	-- 7645, 7644, 
-	-- -- VOID ELF TRAIN
-	-- 95888, 92672, 95682, 92861, 
-	-- -- VUPLERA TRAIN
-	-- 140902, 144035, 141057, 144127, 
-	-- -- WORGEN/GILNEAN TRAIN
-	-- 19357, 23323, 19445, 23318, 19030, 18780,
-	-- -- ZANDALARI TRAIN
-	-- 100215, 126960, 100346, 127334 
--- }
+local myName = "DaDaNMF"
+local myVersion = "v1.2"
 
-local myName = "DollyAndDotAreNotMyFriends"
-local myVersion = "v1.1"
+DaDSoundGroup = { category = "Unknown", name = "Unknown", ids = {} }
 
-local function PrintToFrame(text, isError, frame)
+-- Create a new DaDSoundGroup object with the specified category, group name, and ids
+function DaDSoundGroup:new(category, name, ids)
+	local self = {}
+	self.__index = self
+	self.category = category or "Unknown"
+	self.name = name or "Unknown"
+	self.ids = ids or {}
+	return self
+end
+
+-- Add a single sound ID to the group
+function DaDSoundGroup:add(id)
+	if FindIndex(self.ids, id) == nil then
+		table.insert(self.ids, id)
+	end
+end
+
+-- Add a range of sound IDs to the group
+function DaDSoundGroup:addRange(ids)
+	for _, id in pairs(ids) do
+		self:add(id)
+	end
+end
+
+-- Find the index of a value in the table, returns nil if not found
+-- PARAMETERS
+-- tbl: table to search
+-- value: value to look for in table
+local function FindIndex(tbl, value) 
+	for index, v in pairs(tbl) do
+		if (v == value) then
+			return index
+		end
+	end
+	return nil
+end
+
+function GetTableSize(tbl)
+	if tbl == nil then
+		return nil
+	end
+    local count = 0
+    for _, __ in pairs(tbl) do
+        count = count + 1
+    end
+    return count
+end
+
+-- Colorize the given text using a hex color code, for printing to a chat frame
+-- PARAMETERS
+-- text: text to colorize
+-- hexColor: hex color code
+local function ColorText(text, hexColor)
+	return "\124c"..hexColor..text.."\124r"
+end
+
+-- Print a message to the specified chat frame
+-- PARAMETERS
+-- text: message to print
+-- isError: true to print using error text color, otherwise use default text color
+-- prefix: true to prefix each message with addon name, otherwise no prefix
+-- frame: output chat frame (DEFAULT_CHAT_FRAME if none specified)
+local function PrintToFrame(text, isError, prefix, frame)
 	if frame == nil then
 		frame = DEFAULT_CHAT_FRAME
 	end
 	if isError then
-		frame:AddMessage(myName.." "..tostring(text), 1, 0.2, 0.2)
+		if prefix then
+			frame:AddMessage("["..ColorText(myName, "ffebe834").."] "..tostring(text), 1, 0.2, 0.2)
+		else
+			frame:AddMessage(tostring(text), 1, 0.2, 0.2)
+		end
 	else
-		frame:AddMessage(myName.." "..tostring(text), 0.6, 1, 0.8);
+		if prefix then
+			frame:AddMessage("["..ColorText(myName, "ffebe834").."] "..tostring(text))
+		else
+			frame:AddMessage(tostring(text))
+		end
 	end
 end
 
-local function SimpleFindArrayIndex (array, value) 
-	for index, existingValue in pairs(array) do
-		if (existingValue == value) then
-			return index
-		end
-	end
-	return 0
-end
+local DollyAndDotGroup = DaDSoundGroup:new("toys", "meerah", {1998845, 3169894})
+
+local TrainGroup = DaDSoundGroup:new("toys", "train", {
+	539203, 539219, 1313588, 1306531, 1902030, 1902543,
+	539516, 539730, 539802, 539881, 540271, 540275,
+	542017, 541769, 1730534, 1730908, 540734, 540535,
+	2491898, 2531204, 1731656, 1731282, 1951457, 1951458, 
+	3107182, 3107651, 540947, 540870, 1316209, 1304872,
+	1732405, 1732030, 541157, 541239, 636621, 630296, 
+	630298, 542896, 542818, 543085, 543093, 542600, 542526,
+	1732785, 1733163, 3106252, 3106717, 542206, 542035, 
+	541463, 541601, 1903049, 1903522
+})
+
+local RhoninGroup = DaDSoundGroup:new("npcs", "rhonin", {})
+
+local GroupDictionary = {}
+GroupDictionary["meerah"] = DollyAndDotGroup
+GroupDictionary["train"] = TrainGroup
+--GroupDictionary["rhonin"] = RhoninGroup
+
+local DefaultGroupNames = {"meerah", "train"}
 
 local function MuteAllSounds()
    count = 0
    if DollyAndDotCustomSounds ~= nil then
-      for _, fileDataId in pairs (DollyAndDotCustomSounds) do
+      for _, fileDataId in pairs(DollyAndDotCustomSounds) do
          MuteSoundFile(fileDataId)
          count = count + 1
       end
@@ -137,156 +120,153 @@ local function MuteAllSounds()
    return count
 end
 
-local function RestoreDefaultSoundList()
-	PrintToFrame("set mute list to defaults")
-	DollyAndDotCustomSounds = {}
-	for _, fileDataId in pairs (DollyAndDotIDs) do
-		table.insert(DollyAndDotCustomSounds, fileDataId)
-	end
-	for _, fileDataId in pairs (TrainIDs) do
-		table.insert(DollyAndDotCustomSounds, fileDataId)
+local function SetDefaultSoundList()
+	for _, group in pairs(DefaultGroupNames) do
+		for _, id in pairs(GroupDictionary[group].ids) do
+			if FindIndex(DollyAndDotCustomSounds, id) == nil then
+				table.insert(DollyAndDotCustomSounds, id)
+			end
+		end
 	end
 end
 
-SLASH_DADMUTE1 = "/dadmute"
-SLASH_DADMUTE2 = "/dollymute"
-SLASH_DADMUTE3 = "/mute"
+SLASH_DADMUTE1 = "/mute"
+SLASH_DADMUTE2 = "/dadmute"
 SlashCmdList.DADMUTE = function(command, editBox)
 	if command == "default" then
 		if DollyAndDotCustomSounds ~= nil then
-			for _, id in pairs (DollyAndDotCustomSounds) do
+			PrintToFrame("resetting to default mute list", false, true)
+			for _, id in pairs(DollyAndDotCustomSounds) do
 				UnmuteSoundFile(id)
 			end
 		end
-		RestoreDefaultSoundList()
+		DollyAndDotCustomSounds = {}
+		SetDefaultSoundList()
+		for _, group in pairs(DefaultGroupNames) do
+			PrintToFrame("added "..ColorText(group, "ffb8cff5").." to mute list", false, true)
+		end
 		local count = MuteAllSounds()
-		PrintToFrame("muted "..count.." total sounds. :)")
+		PrintToFrame("muted "..ColorText(count, "ffb8cff5").." total sounds :)", false, true)
 		return
-	elseif command == "meerah" then
-		for _, id in pairs (DollyAndDotIDs) do
-			if SimpleFindArrayIndex(DollyAndDotCustomSounds, id) == 0 then
+	elseif GroupDictionary[command] ~= nil then
+		for _, id in pairs(GroupDictionary[command].ids) do
+			if FindIndex(DollyAndDotCustomSounds, id) == nil then
 				table.insert(DollyAndDotCustomSounds, id)
 				MuteSoundFile(id)
 			end
 		end
-		PrintToFrame("added Dolly and Dot sounds to mute list")
-		return
-	elseif command == "train" then
-		for _, id in pairs (TrainIDs) do
-			if SimpleFindArrayIndex(DollyAndDotCustomSounds, id) == 0 then
-				table.insert(DollyAndDotCustomSounds, id)
-				MuteSoundFile(id)
-			end
-		end
-		PrintToFrame("added Train sounds to mute list")
+		PrintToFrame("added "..ColorText(command, "ffb8cff5").." sound group to mute list", false, true)
 		return
 	end
 	
 	local fileDataId = tonumber(command)
 	if fileDataId == nil then
-		PrintToFrame("file data id must be a number", true)
+		PrintToFrame("file data id must be a number", true, true)
 		return
 	end
-
-	if DollyAndDotCustomSounds == nil then
-		DollyAndDotCustomSounds = {}
-	end
 	
-	local index = SimpleFindArrayIndex(DollyAndDotCustomSounds, tonumber(fileDataId))
-	
-	if index > 0 then
-		PrintToFrame("file data id "..fileDataId.." is already muted!")
+	local index = FindIndex(DollyAndDotCustomSounds, tonumber(fileDataId))
+	if index ~= nil then
+		PrintToFrame("file data id "..ColorText(fileDataId, "ffb8cff5").." is already muted!", false, true)
 	else
 		table.insert(DollyAndDotCustomSounds, tonumber(fileDataId))
 		MuteSoundFile(fileDataId)
-		PrintToFrame("muted file data id "..fileDataId)
+		PrintToFrame("muted file data id "..ColorText(fileDataId, "ffb8cff5"), false, true)
 	end
 end
 
-SLASH_DADUNMUTE1 = "/dadunmute"
-SLASH_DADUNMUTE2 = "/dollyunmute"
-SLASH_DADUNMUTE3 = "/unmute"
+SLASH_DADUNMUTE1 = "/unmute"
+SLASH_DADUNMUTE2 = "/dadunmute"
 SlashCmdList.DADUNMUTE = function(command, editBox)
-	if command == "meerah" then
-		for _, id in pairs (DollyAndDotIDs) do
-			local index = SimpleFindArrayIndex(DollyAndDotCustomSounds, id)
-			if index > 0 then
+	if GroupDictionary[command] ~= nil then
+		for _, id in pairs(GroupDictionary[command].ids) do
+			local index = FindIndex(DollyAndDotCustomSounds, id)
+			if index ~= nil then
 				table.remove(DollyAndDotCustomSounds, index)
 				UnmuteSoundFile(id)
 			end
 		end
-		PrintToFrame("removed Dolly and Dot sounds from mute list")
-		return
-	elseif command == "train" then
-		for _, id in pairs (TrainIDs) do
-			local index = SimpleFindArrayIndex(DollyAndDotCustomSounds, id)
-			if index > 0 then
-				table.remove(DollyAndDotCustomSounds, index)
-				UnmuteSoundFile(id)
-			end
-		end
-		PrintToFrame("removed Train sounds from mute list")
+		PrintToFrame("removed "..ColorText(command, "ffb8cff5").." sound group from mute list", false, true)
 		return
 	end
 	
 	local fileDataId = tonumber(command)
 	if tonumber(fileDataId) == nil then
-		PrintToFrame("file data id must be a number", true)
+		PrintToFrame("must enter a recognized sound group or a numeric sound id", true, true)
 		return
 	end
-	
-	if DollyAndDotCustomSounds == nil then
-		DollyAndDotCustomSounds = {}
-		PrintToFrame("file data id "..fileDataId.." was not muted!")
-		return
-	end
-	
-	local index = SimpleFindArrayIndex(DollyAndDotCustomSounds, tonumber(fileDataId))
-	
-	if index > 0 then
+		
+	local index = FindIndex(DollyAndDotCustomSounds, tonumber(fileDataId))
+	if index ~= nil then
 		table.remove(DollyAndDotCustomSounds, index)
 		UnmuteSoundFile(fileDataId)
-		PrintToFrame("unmuted file data id "..fileDataId)
+		PrintToFrame("unmuted id "..ColorText(fileDataId, "ffb8cff5"), false, true)
 	else
-		PrintToFrame("file data id "..fileDataId.." was not muted!")
+		PrintToFrame("id "..ColorText(fileDataId, "ffb8cff5").." was not muted!", false, true)
 	end			
 end
 
 SLASH_DADMUTELIST1 = "/mutelist"
-SlashCmdList.DADMUTELIST = function(editBox)
-	if DollyAndDotCustomSounds == nil then
-		DollyAndDotCustomSounds = {}
-	end
-	
+SlashCmdList.DADMUTELIST = function(editBox)	
 	local defaultCount = 0
 	local customCount = 0
 	for _, id in pairs(DollyAndDotCustomSounds) do
-		if SimpleFindArrayIndex(DollyAndDotIDs, id) > 0 then
-			defaultCount = defaultCount + 1
-		elseif SimpleFindArrayIndex(TrainIDs, id) > 0 then
-			defaultCount = defaultCount + 1
-		else
+		local isDefault = false
+		for _, group in pairs(DefaultGroupNames) do
+			if FindIndex(GroupDictionary[group].ids, id) ~= nil and not isDefault then
+				defaultCount = defaultCount + 1
+				isDefault = true
+			end
+		end
+		if not isDefault then
 			customCount = customCount + 1
 		end
 	end
-	PrintToFrame(defaultCount.." default sounds and "..customCount.." additional sounds muted")
+	PrintToFrame(ColorText(defaultCount, "ffb8cff5").." default sounds and "..ColorText(customCount, "ffb8cff5").." additional sounds muted", false, true)
+end
+
+SLASH_DADMUTEHELP1 = "/mutehelp"
+SlashCmdList.DADMUTEHELP = function(command, editBox)
+	if command == nil or command == "" then
+		PrintToFrame("help topics: mute, unmute, groups. Type "..ColorText("/mutehelp <topic>", "ffebe834").." for more info (e.g., /mutehelp mute)", false, true)
+	elseif command == "mute" then
+		PrintToFrame("Mutes a group of sounds by name, or one sound file id", false)
+		PrintToFrame("Usage: "..ColorText("/mute default", "ffebe834")..", "..ColorText("/mute <group name>", "ffebe834")..", or "..ColorText("/mute <sound id>", "ffebe834"), false)
+	elseif command == "unmute" then
+		PrintToFrame("Unmutes a group of sounds by name, or one sound file id", false)
+		PrintToFrame("Usage: "..ColorText("/unmute <group name>", "ffebe834").." or "..ColorText("/unmute <sound id>", "ffebe834"), false)
+	elseif command == "groups" then
+		local groupNames = ""
+		for key, group in pairs(GroupDictionary) do
+			if groupNames == "" then
+				groupNames = key
+			else
+				groupNames = groupNames..", "..key
+			end
+		end
+		PrintToFrame("Group names recognized by mute and unmute commands: "..groupNames, false)
+	else
+		PrintToFrame(command.." is not a recognized command. Try "..ColorText("/mutehelp mute", "ffebe834")..", "..ColorText("/mutehelp unmute", "ffebe834")..", or "..ColorText("/mutehelp groups", "ffebe834"))
+	end
 end
 
 local DADFrame = CreateFrame("Frame", "DollyAndDotAreNotMyFriends")
 DADFrame:RegisterEvent("PLAYER_LOGIN")
+DADFrame:RegisterEvent("ADDON_LOADED")
 
 DADFrame:SetScript("OnEvent",
-	function(self, event, ...)
-		if event == "PLAYER_LOGIN" then
-			if DollyAndDotCustomSounds == nil or DollyAndDotAreNotMyFriends == {} then
-				RestoreDefaultSoundList()
+	function(self, event, arg1)
+		if event == "ADDON_LOADED" and arg1 == "DollyAndDotAreNotMyFriends" then			
+			if not DollyAndDotCustomSounds or not DollyAndDotSet then
+				if not DollyAndDotCustomSounds then
+					DollyAndDotCustomSounds = {}
+				end
+				DollyAndDotSet = true
+				SetDefaultSoundList()
 			end
-			local customCount = table.getn(DollyAndDotCustomSounds)
-			if customCount > 0 then
-				PrintToFrame(myVersion.." preferences loaded ... "..customCount.." sounds blacklisted.")
-			end
+		elseif event == "PLAYER_LOGIN" then
 			local count = MuteAllSounds()
-			PrintToFrame(myVersion.." muted "..count.." total sounds. Enjoy! :)")
+			PrintToFrame(myVersion.." loaded ... "..ColorText(count, "ffb8cff5").." sounds blacklisted. Type "..ColorText("/mutehelp", "ffebe834").." for more info!", false, true)
 		end
 	end
 )
